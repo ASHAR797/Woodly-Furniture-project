bea17d9e1e10a9ebe9d63bb60ef510050aad4893
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2024-04-29 18:22:26 +0500

    Added files

---
+++ app.js
@@ -0,0 +1,28 @@
+var homeOne = document.querySelector("#home-one")
+let homeTwo = document.querySelector("#home-two")
+let homeThree = document.querySelector("#home-three")
+let homeFour = document.querySelector("#home-four")
+
+// Home One page
+
+homeOne.addEventListener("click", function () {
+    window.location.href = "https://google.com";
+});
+
+// Home Two page
+
+homeTwo.addEventListener("click", function () {
+    window.location.href = "https://google.com";
+});
+
+// Home Three page
+
+homeThree.addEventListener("click", function () {
+    window.location.href = "https://google.com";
+});
+
+// Home Four page
+
+homeFour.addEventListener("click", function () {
+    window.location.href = "https://google.com";
+});
